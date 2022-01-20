6480c8503d65867eec516faa9a8e190a26996ae8
joshua <joshuasend1@gmail.com>
2022-01-20 10:34:39 +0000

    Load schema and rules separately

--- schema/initialise.py
+++ schema/initialise.py
@@ -31,13 +31,18 @@ def initialise_database(uri, database, force=False):
             raise ValueError(f"Database '{database}' already exists")
     client.databases().create(database)
     session = client.session(database, SessionType.SCHEMA)
+    with open("schema/cti-schema.tql", "r") as schema_file:
+        schema = schema_file.read()
+    with open("schema/cti-rules.tql", "r") as rules_file:
+        rules = rules_file.read()
     print('.....')
-    print('Inserting schema...')
+    print('Inserting schema and rules...')
     print('.....')
-    with open("schema/cti-schema.tql", "r") as typeql_file:
-        schema = typeql_file.read()
     with session.transaction(TransactionType.WRITE) as write_transaction:
         write_transaction.query().define(schema)
+        write_transaction.commit()
+    with session.transaction(TransactionType.WRITE) as write_transaction:
+        write_transaction.query().define(rules)
         write_transaction.commit()
     print('.....')
     print('Successfully committed schema!')
